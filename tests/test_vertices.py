7b47efee6556cddfac95c3c33c51e07ee8f11566
Konstantin Malyshev <konstantin.a.malyshev@gmail.com>
2026-01-16 20:04:31 +0100

    first mcp unit test added

--- tests/test_vertices.py
+++ tests/test_vertices.py
@@ -1,5 +1,27 @@
+import json
 import pytest
+import pytest_asyncio
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
-from theo_mcp_server.tools import get_verse_by_caption
+server_params = StdioServerParameters(
+    command="theo-mcp",
+    # args=["run", "server", "fastmcp_quickstart", "stdio"],  # We're already in snippets dir
+    # env={"UV_INDEX": os.environ.get("UV_INDEX", "")},
+)
 
+@pytest_asyncio.fixture
+async def mcp_session():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            yield session
 
+@pytest.mark.asyncio
+async def test_get_verse_by_caption(mcp_session):
+    result = await mcp_session.call_tool("get_verse_by_caption", {"caption": "Jn 1:1"})
+    dict = result.structuredContent
+
+    print(json.dumps(dict, indent=2, ensure_ascii=False))
+
+    assert dict.get('caption') == "Jn 1:1"
